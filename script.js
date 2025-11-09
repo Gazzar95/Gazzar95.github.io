0c3abb3a668b94cae16184c0f8caddffdb1458e1
Omar El Gazzar <gazzar95@yahoo.com>
2025-11-08 16:43:30 -0800

    copy of njkaiser's portifolio

--- script.js
+++
@@ -1,9 +0,0 @@
-const navToggle = document.querySelector(".nav-toggle");
-const nav = document.querySelector(".nav");
-
-navToggle.addEventListener("click", () => {
-    nav.classList.toggle("open");
-});
-
-document.getElementById("year").textContent = new Date().getFullYear();
-
